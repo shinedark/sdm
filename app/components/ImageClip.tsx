1844b629f44db8458ce3e1907ab20d7965fe443c
Shine Dark <mide0303@gmail.com>
2024-08-02 07:48:09 -0500

    start

---
+++ app/components/ImageClip.tsx
@@ -0,0 +1,34 @@
+// app/components/ImageClip.tsx
+
+'use client'; // Add this directive to make this component a Client Component
+
+import React from 'react';
+
+const ImageClip: React.FC = () => {
+  return (
+    <div className="relative flex items-center justify-center ">
+      <picture className="w-64 aspect-[1/cos(30deg)] cursor-pointer">
+        <img
+          rel="prefetch"    
+          src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjRqcHBzc21odm96dTd2bXFuOW5tN2UxandxOXRlbmxjdGs5YjdkNiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3ohs4CacylzFaHjMM8/giphy.gif" // Replace with your image path
+          alt="Clipped"
+          className="w-full h-full object-cover transition-[--b] duration-600"
+          style={{
+            clipPath: `polygon(50% 0, 100% 100%, 0 100%, 50% 0, 50% var(--b), calc(var(--b)*cos(30deg)) calc(100% - var(--b)/2), calc(100% - var(--b)*cos(30deg)) calc(100% - var(--b)/2), 50% var(--b))`,
+            background: 'linear-gradient(45deg, #FA6900, #C02942)',
+          }}
+        />
+      </picture>
+      <style jsx>{`
+        :global(img) {
+          --b: 25px; /* Initial border thickness */
+        }
+        picture:hover img {
+          --b: 144px; /* Change border thickness on hover */
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default ImageClip;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,28 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      borderStyle: {
+        outset: 'outset',
+      },
+      boxShadow: {
+        custom: '-1px -1px 0px 3px rgba(0, 0, 0, 1)',
+      },
+      keyframes: {
+        rotateLeft: {
+          '0%': { transform: 'rotate(0deg)' },
+          '33%': { transform: 'rotate(-180deg)' },
+          '67%': { transform: 'rotate(-270deg)' },
+          '100%': { transform: 'rotate(-360deg)' }, // Rotate left
+        },
+        rotateRight: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(150deg)' }, // Rotate right
+        },
+      },
+      animation: {
+        rotateLeft: 'rotateLeft 0.5s ease-in-out',
+        rotateRight: 'rotateRight 0.5s ease-in-out',
+      },
     },
   },
   plugins: [],
