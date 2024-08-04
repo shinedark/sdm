974a77a9b90116c1f3b744169338c26d6a54858b
Shine Dark <mide0303@gmail.com>
2024-08-04 08:32:21 -0500

    fix prop

--- app/components/Cube.tsx
+++ app/components/Cube.tsx
@@ -4,7 +4,12 @@ import React, { useState } from 'react';
 import './Cube.css'; // Import the CSS
 import Monsty from './Monsty';
 
-const Cube = ({ isAnimating }) => {
+// Define prop types
+interface CubeProps {
+  isAnimating: boolean;
+}
+
+const Cube: React.FC<CubeProps> = ({ isAnimating }) => {
   const [rotateX, setRotateX] = useState(0);
   const [rotateY, setRotateY] = useState(0);
   const [step, setStep] = useState(0); // To keep track of the rotation sequence
@@ -34,11 +39,11 @@ const Cube = ({ isAnimating }) => {
     <div className={`scene ${isAnimating ? 'cube-bounce' : ''}`} onClick={handleCubeClick}>
       <div className="cube" style={{ transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)` }}>
         <div className="cube__face cube__face--front"><Monsty isAnimating={isAnimating} key={1} /></div>
-        <div className="cube__face cube__face--back"><Monsty isAnimating={isAnimating}  key={2} /></div>
-        <div className="cube__face cube__face--right"><Monsty isAnimating={isAnimating} key={3}  /></div>
-        <div className="cube__face cube__face--left"><Monsty isAnimating={isAnimating} key={4}  /></div>
-        <div className="cube__face cube__face--top"><Monsty isAnimating={isAnimating}  key={5} /></div>
-        <div className="cube__face cube__face--bottom"><Monsty isAnimating={isAnimating} key={6}  /></div>
+        <div className="cube__face cube__face--back"><Monsty isAnimating={isAnimating} key={2} /></div>
+        <div className="cube__face cube__face--right"><Monsty isAnimating={isAnimating} key={3} /></div>
+        <div className="cube__face cube__face--left"><Monsty isAnimating={isAnimating} key={4} /></div>
+        <div className="cube__face cube__face--top"><Monsty isAnimating={isAnimating} key={5} /></div>
+        <div className="cube__face cube__face--bottom"><Monsty isAnimating={isAnimating} key={6} /></div>
       </div>
     </div>
   );
