dc98c95d1466f5c350c8245a5767ef0414574c5c
SHINE DARK <mide0303@gmail.com>
2025-06-18 20:13:51 -0600

    finx pinanata

--- lib/pinata.ts
+++ lib/pinata.ts
@@ -1,12 +1,12 @@
-import { PinataSDK } from 'pinata';
+// import { PinataSDK } from 'pinata';
 
-if (!process.env.PINATA_JWT) {
-  throw new Error('PINATA_JWT environment variable is not set');
-}
+// if (!process.env.PINATA_JWT) {
+//   throw new Error('PINATA_JWT environment variable is not set');
+// }
 
-export const pinata = new PinataSDK({
-  pinataJwt: process.env.PINATA_JWT!,
-  pinataGateway: process.env.PINATA_GATEWAY!, // e.g., "fun-llama-300.mypinata.cloud"
-});
+// export const pinata = new PinataSDK({
+//   pinataJwt: process.env.PINATA_JWT!,
+//   pinataGateway: process.env.PINATA_GATEWAY!, // e.g., "fun-llama-300.mypinata.cloud"
+// });
 
 
